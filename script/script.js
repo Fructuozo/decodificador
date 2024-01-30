3c96feb6bf103892c73fb81eac80cecb15244304
Gabriel Fructuozo <gabrielfruc@gmail.com>
2024-01-30 15:41:01 -0300

    projetoFinal

---
+++ script/script.js
@@ -0,0 +1,29 @@
+var textInput = document.querySelector('#text');
+var textOutput = document.querySelector('#output');
+
+function criptografar() {
+    var texto = textInput.value;
+
+    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="text">' + resultCripto + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
+
+function descriptografar() {
+    
+    var texto = textInput.value;
+
+    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+  
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
+
+function copiar() {
+    var textoCop = document.getElementById('input-texto');
+  
+    textoCop.select();
+    document.execCommand('copy');
+    alert("Texto copiado.");
+}
